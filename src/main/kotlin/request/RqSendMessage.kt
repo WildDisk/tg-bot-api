abb1d71730f7055dc7a67060998977d5fdc2512c
WildDisk <ivan.zolotopup@gmail.com>
2023-07-08 20:44:49 +1000

    first commit

--- src/main/kotlin/request/RqSendMessage.kt
+++ src/main/kotlin/request/RqSendMessage.kt
@@ -2,9 +2,9 @@ package request
 
 import core.TgClient
 import io.ktor.serialization.*
-import model.Structure
 import model.Error
-import model.GetMe
+import model.SendMessage
+import model.Structure
 
 class RqSendMessage(
     private val client: TgClient,
@@ -12,7 +12,7 @@ class RqSendMessage(
     private val text: String
 ) : Request {
     override suspend fun send(): Structure = try {
-        this.client.request<GetMe>("sendMessage?chat_id=$chatId&text=$text")
+        this.client.request<SendMessage>("sendMessage?chat_id=$chatId&text=$text")
     } catch (e: JsonConvertException) {
         this.client.request<Error>("sendMessage?chat_id=$chatId&text=$text")
     } catch (e: Exception) {
